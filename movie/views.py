013a715291a8cc96cd3121a332b2c0c3aff295d3
JuanJoseBotero <jjboterog@eafit.edu.co>
2024-08-01 18:01:04 -0500

    Sistema de filtro de busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -9,7 +9,10 @@ def home(request):
     """ return render(request, 'home.html') """
     """ return render(request, 'home.html', {'name' : 'Juan Jose Botero'}) """
     searchTerm = request.GET.get('searchMovie')
-    movies = Movie.objects.all()
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'home.html', {'seatchTerm':searchTerm, 'movies': movies})
     
 def about(request):
