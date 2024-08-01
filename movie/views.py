7749bcc5f8fa8865ca07cd5e9bec3f32b20dc192
JuanJoseBotero <jjboterog@eafit.edu.co>
2024-08-01 11:20:14 -0500

    cambio de nombre en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 def home(request):
     """ return HttpResponse('<h1>Welcome to my Home Page</h1>') """
     """ return render(request, 'home.html') """
-    return render(request, 'home.html', {'name' : 'Juan Jose'})
+    return render(request, 'home.html', {'name' : 'Juan Jose Botero'})
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
