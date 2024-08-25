346afd59a821bbb1582093f452429ecf6ab947c1
JuanJoseBotero <jjboterog@eafit.edu.co>
2024-08-25 17:57:12 -0500

    Creacion de estadisticas y signup, configuracion de estas en urls y views

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Movie
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 # Create your views here.
 
 def home(request):
-    """ return HttpResponse('<h1>Welcome to my Home Page</h1>') """
-    """ return render(request, 'home.html') """
-    """ return render(request, 'home.html', {'name' : 'Juan Jose Botero'}) """
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
@@ -18,3 +18,70 @@ def home(request):
 def about(request):
     """ return HttpResponse('<h1>Welcome to About Page</h1>') """
     return render(request, 'about.html')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
+
+def statistics_view(request):
+    # Obtener todas las películas
+    all_movies = Movie.objects.all()
+
+    # Crear diccionarios para almacenar la cantidad de películas por año y por género
+    movie_counts_by_year = {}
+    movie_counts_by_genre = {}
+
+    # Filtrar las películas por año y género
+    for movie in all_movies:
+        year = movie.year if movie.year else "None"
+        genre = movie.genre.split(',')[0] if movie.genre else "None"
+        
+        movie_counts_by_year[year] = movie_counts_by_year.get(year, 0) + 1
+        movie_counts_by_genre[genre] = movie_counts_by_genre.get(genre, 0) + 1
+
+    # Gráfica 1: Películas por año
+    plt.figure()
+    bar_width = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+
+    # Guardar la gráfica en un objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    # Convertir la gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png).decode('utf-8')
+
+    # Gráfica 2: Películas por género
+    plt.figure()
+    bar_positions = range(len(movie_counts_by_genre))
+    plt.bar(bar_positions, movie_counts_by_genre.values(), width=bar_width, align='center')
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+
+    # Guardar la gráfica en un objeto BytesIO
+    buffer2 = io.BytesIO()
+    plt.savefig(buffer2, format='png')
+    buffer2.seek(0)
+    plt.close()
+
+    # Convertir la gráfica a base64
+    image_png2 = buffer2.getvalue()
+    buffer2.close()
+    graphic2 = base64.b64encode(image_png2).decode('utf-8')
+
+    # Renderizar la plantilla statistics.html con ambas gráficas
+    return render(request, 'statistics.html', {'graphic': graphic, 'graphic2': graphic2})
+   
